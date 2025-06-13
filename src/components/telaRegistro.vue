981249d0dedd469a42b47e36184ef93e5f1f0aa9
goodsir6 <eduardo.bomsenhor.info@gmail.com>
2025-06-13 13:45:19 -0300

    feat: início telas registro #4

---
+++ src/components/telaRegistro.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+<template>
+    <div class="conteudo">
+        <div><img src="/public/imgsRegistro/imgRegistro.png" style="width: 500px; height: 500px;"></div>
+        <div>form</div>
+    </div>
+</template>
+
+<style scoped>
+.conteudo{
+    display: flex;
+    justify-content: space-around;
+    align-content: center;
+}
+</style>
