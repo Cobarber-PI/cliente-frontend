719151c061d81d8a2f4f3e08f3a73dc6de16cfa6
Henrique0330 <henriquebelboni@gmail.com>
2025-06-22 21:40:08 -0300

    feat: Adaptando ambiente, criação da rota e componente 'introdução' #10

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegistroView from '../views/telaRegistroView.vue'
+import IntroducaoView from '../views/introducaoView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +15,11 @@ const router = createRouter({
       path: '/registro',
       name: 'registro',
       component: RegistroView,
+    },
+    {
+      path: '/introducao',
+      name: 'introducao',
+      component: IntroducaoView
     },
     {
       path: '/about',
---
+++ src/views/introducaoView.vue
@@ -0,0 +1,7 @@
+<script setup>
+import introducao from '@/components/introducao.vue';
+</script>
+
+<template>
+    <introducao />
+</template>
