7f74148d1274ce220795ead0cf5a9999d75ddb67
Henrique0330 <henriquebelboni@gmail.com>
2025-06-22 23:09:51 -0300

    feat: Colocando imagem e definindo seu posicionamento #10

--- src/components/introducao.vue
+++ src/components/introducao.vue
@@ -1,7 +1,43 @@
 <script setup></script>
 
 <template>
-    <h1>os guri</h1>
+  <div class="container">
+    <div class="imageFundo"></div>
+    <div class="content">
+      <h1>opa</h1>
+    </div>
+  </div>
+  <div class="teste">
+    <h1>teste</h1>
+    <p>opaaaa</p>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+}
+
+.imageFundo {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-image: url('/introdução/nathon-oski-EW_rqoSdDes-unsplash-scaled.jpg');
+  background-size: cover;
+  background-position: center;
+  filter: blur(0.5px) brightness(0.2);
+}
+
+.content {
+  position: relative;
+  color: white;
+}
+
+.teste {
+  background-color: red;
+  width: 200px;
+  height: 200px;
+}
+</style>
