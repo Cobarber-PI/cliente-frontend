f12524f7e0c87d2f3af312c5d783ac5f812ca3b7
marcoperuzzzocmd <marcoantperuzzo@gmail.com>
2025-08-28 00:24:11 -0300

    feat: feita a parte de input #16

--- src/components/TelaHome.vue
+++ src/components/TelaHome.vue
@@ -12,7 +12,7 @@
                 <ul>
                     <router-link to="/">Início</router-link>
                     <router-link to="/barbearias">Barbearias</router-link>
-                    <router-link to="/agendamentos">Agendamentos</router-link>  
+                    <router-link to="/agendamentos">Agendamentos</router-link>
                 </ul>
             </div>
             <div class="login">
@@ -21,7 +21,7 @@
                     <p>Login</p>
                 </button>
                 <button class="cadastrar">
-                    Cadastrar   
+                    Cadastrar
                 </button>
             </div>
         </div>
@@ -31,7 +31,13 @@
         <h1>Encontre a <span>barbearia perfeita</span></h1>
         <p>Agende um horário com os melhores barbeiros de sua região. Simples, rápido e sem complicação</p>
         <div class="busca">
-
+            <div class="inputs">
+                <input type="text" placeholder="Digite seu bairro ou cidade">
+                <button class="buscarBarberias">
+                    <img src="/public/home/lupa.svg" alt="">
+                    <p>Buscar barbearias</p>
+                </button>
+            </div>
         </div>
     </main>
     <!--  calc(100vh - 80px)-->
@@ -39,6 +45,7 @@
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap');
+
 header {
     width: 100%;
     height: 80px;
@@ -48,7 +55,7 @@ header {
     align-items: center;
 }
 
-.login{
+.login {
     display: flex;
     gap: 10px;
 }
@@ -62,18 +69,18 @@ header {
     width: 90%;
 }
 
-.logo{
+.logo {
     display: flex;
     align-items: center;
     gap: 10px;
 }
 
-.logo img{
+.logo img {
     margin-top: 15px;
     width: 50%;
-}   
+}
 
-.logo h1{
+.logo h1 {
     font-size: 24px;
     font-family: "Cinzel", serif;
     font-weight: bold;
@@ -88,11 +95,12 @@ ul {
     justify-content: center;
 }
 
-a{
+a {
     text-decoration: none;
     color: #B0B0B0;
 }
-button{
+
+.entrar {
     display: flex;
     background-color: #09090B;
     color: white;
@@ -106,29 +114,39 @@ button{
     gap: 11px;
     cursor: pointer;
 }
-.cadastrar{
+
+.cadastrar {
     background-color: #5f5136;
     border: 1px solid #09090B;
-
+    color: white;
+    border-radius: 10px;
+    width: 95px;
+    height: 40px;
+    font-size: 15px;
+    cursor: pointer;
 }
-main{
+
+main {
     width: 100%;
-    height: 699px; 
+    height: 699px;
     background-image: url('/public/home/fundoHome.svg');
     background-size: cover;
 }
-main > h1{
+
+main>h1 {
     color: white;
     font-size: 75px;
     text-align: center;
     padding-top: 95px;
     font-family: 'monteserrat', sans-serif;
 }
-main > h1 span{
+
+main>h1 span {
     color: #F1C23C;
     font-weight: bold;
 }
-main > p{
+
+main>p {
     color: #A1A1AA;
     font-size: 25px;
     text-align: center;
@@ -136,7 +154,8 @@ main > p{
     margin: auto;
     margin-top: 25px;
 }
-.busca{
+
+.busca {
     width: 850px;
     height: 120px;
     background-color: #1F1F21;
@@ -144,5 +163,38 @@ main > p{
     margin-top: 60px;
     border-radius: 15px;
     border: solid 1px #F1C23C;
+    display: flex;
+    align-items: center;
+}
+.inputs {
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+    width: 100%;
+}
+.inputs > input{
+    width: 510px;
+    height: 65px;
+    border-radius: 15px;
+    border: solid 1px #F1C23C;
+    padding-left: 15px;
+    font-size: 15px;
+    background-color: #131315;
+    color: white;
+    font-family: 'Inter', sans-serif;
+}
+.inputs > .buscarBarberias{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 15px;
+    background-color: #F1C23C;
+    border: none;
+    border-radius: 15px;
+    width: 250px;
+    height: 65px;
+    cursor: pointer;
+    font-size: 18px;
+    font-weight: 500;
 }
 </style>
