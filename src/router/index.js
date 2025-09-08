1e57c8430aa70e151138412439d3dd0d0b781f6c
Henrique0330 <henriquebelboni@gmail.com>
2025-09-08 13:11:26 -0300

    feat: add home in the router

--- src/router/index.js
+++ src/router/index.js
@@ -27,7 +27,12 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue'),
-    }, 
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: HomeView,
+    }
   ],
 })
 
