65aefdd33dba520afef8ecc5eb290b9b18a2158c
marcoperuzzocmd <marcoantperuzzo@gmail.com>
2025-10-06 23:25:38 -0300

    feat: começando o componente

--- src/components/Escolha.vue
+++ src/components/Escolha.vue
@@ -1,8 +1,53 @@
 <script setup></script>
 
 <template>
+    <h1>Bem vindo!</h1>
+    <p>Como você gostaria de usar nossa plataforma?</p>
+    <div class="container">
+        <div class="escolha">
+
+        </div>
+        <div class="escolha">
+
+        </div>
+    </div>
 </template>
 
 <style scoped>
+.textos-cima {
+    margin-top: 70px;
+}
+h1{
+    margin-top: 70px;
+}
+p{
+    gap: 10px;
+    margin: 25px;
+}
+h1,
+p {
+    color: white;
+    text-align: center;
+}
 
+.container {
+    width: 60%;
+    height: 430px;
+    margin: auto;
+    display: flex;
+    background-color: aqua;
+    align-items: center;
+    justify-content: space-between;
+    border-radius: 15px;
+    gap: 15px;
+    margin-top: 20px;
+}
+
+.escolha {
+    background-color: blue;
+    height: 100%;
+    width: 48%;
+    display: flex;
+    border-radius: 15px;
+}
 </style>
