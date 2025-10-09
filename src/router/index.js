e462de6b2e82cb8199886fe87d76aabefcca20fd
Henrique0330 <henriquebelboni@gmail.com>
2025-10-09 20:28:25 -0300

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,6 @@ import LoginView from '../views/LoginView.vue'
 import telaRegistroView from '../views/telaRegistroView.vue'
 import IntroducaoView from '../views/introducaoView.vue'
 import HomeView from '@/views/HomeView.vue'
-import ServicosBarbeirosView from '@/views/servicosBarbeirosView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
