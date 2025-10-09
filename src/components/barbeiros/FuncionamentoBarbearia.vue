08250068fb75194a8077da3777eaa675b2a3a9be
Henrique0330 <henriquebelboni@gmail.com>
2025-10-08 23:06:42 -0300

    feat: Criated Funcionamento and comodidade.  #34

--- src/components/barbeiros/FuncionamentoBarbearia.vue
+++ src/components/barbeiros/FuncionamentoBarbearia.vue
@@ -1,32 +1,36 @@
-<script setup></script>
+<script setup>
+  import MdiClockOutline from 'vue-material-design-icons/ClockOutline.vue';
+</script>
 
 <template>
   <div class="container">
     <div class="funcionamento">
-      <h1 class="title">Horários de Funcionamento</h1>
+  <h1 class="title"><MdiClockOutline class="title-icon" />Horários de Funcionamento</h1>
       <table>
-        <tr>
-          <td><strong>Segunda-feira</strong></td>
-          <td>08:00 - 18:00</td>
-          <td><strong>Sexta-feira</strong></td>
-          <td>08:00 - 19:00</td>
-        </tr>
-        <tr>
-          <td><strong>Terça-feira</strong></td>
-          <td>08:00 - 18:00</td>
-          <td><strong>Sábado</strong></td>
-          <td>08:00 - 19:00</td>
-        </tr>
-        <tr>
-          <td><strong>Quarta-feira</strong></td>
-          <td>08:00 - 18:00</td>
-          <td><strong>Domingo</strong></td>
-          <td>Fechado</td>
-        </tr>
-        <tr>
-          <td><strong>Quinta-feira</strong></td>
-          <td>08:00 - 18:00</td>
-        </tr>
+        <tbody>
+          <tr>
+            <td><strong>Segunda-feira</strong></td>
+            <td>08:00 - 18:00</td>
+            <td><strong>Sexta-feira</strong></td>
+            <td>08:00 - 19:00</td>
+          </tr>
+          <tr>
+            <td><strong>Terça-feira</strong></td>
+            <td>08:00 - 18:00</td>
+            <td><strong>Sábado</strong></td>
+            <td>08:00 - 19:00</td>
+          </tr>
+          <tr>
+            <td><strong>Quarta-feira</strong></td>
+            <td>08:00 - 18:00</td>
+            <td><strong>Domingo</strong></td>
+            <td class="fechado" >Fechado</td>
+          </tr>
+          <tr>
+            <td><strong>Quinta-feira</strong></td>
+            <td>08:00 - 18:00</td>
+          </tr>
+        </tbody>
       </table>
     </div>
     <div class="comodidades">
@@ -45,8 +49,13 @@
 <style scoped>
 
 .title {
+    font-size: 30px;
   margin-left: 2rem;
   margin-top: 1rem;
+}
+
+.title-icon {
+  margin-right: 8px;
 }
 
 .container {
@@ -77,5 +86,31 @@
   background-color: #0F0F0F;
   border-radius: 8px;
   border: solid #262626 1px;
+}
+
+.comodidades ul {
+  padding: 10px 55px;
+  list-style-type: disc;
+}
+
+.comodidades li {
+  margin-bottom: 2px;
+  font-size: 16px;
+}
+
+.comodidades li::marker {
+  color: #F0C238;
+  font-size: 25px;
+}
+
+.comodidades h1 {
+  font-size: 30px;
+  margin-left: 2rem;
+  margin-top: 1rem;
+}
+
+.fechado {
+  color: red;
+  font-weight: bold;
 }
 </style>
