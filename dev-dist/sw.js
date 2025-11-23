a4f8fb7b79672c56d594839fac9608e52b555ed6
marcoperuzzocmd <marcoantperuzzo@gmail.com>
2025-11-23 16:33:36 -0300

    f

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.d9ap5ad8ato"
+    "revision": "0.vktn22988f"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
